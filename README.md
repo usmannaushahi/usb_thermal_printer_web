710a9b6409c6f18c1fcf31a3a3b23d1b5ba7dd2b
Tech Dukkantek <techdukkantek@Techs-MacBook-Pro.local>
2023-03-01 14:11:17 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,84 @@
+<!--
+This README describes the package. If you publish this package to pub.dev,
+this README's contents appear on the landing page for your package.
+
+For information about how to write a good package README, see the guide for
+[writing package pages](https://dart.dev/guides/libraries/writing-package-pages).
+
+For general information about developing packages, see the Dart guide for
+[creating packages](https://dart.dev/guides/libraries/create-library-packages)
+and the Flutter guide for
+[developing packages and plugins](https://flutter.dev/developing-packages).
+-->
+
+This package helps the you to connect to your thermal usb printer via USB in Flutter Web and print. It has been tested in MP583 thermal printer.
+
+
+## Features
+
+printText: It allows you to print a simple text. You can center align or bold the text if you want to.
+
+printEmptyLine: It prints an empty line to create add a space between two printed statements
+
+printDottedLine: It prints a dotted line or you can use it as a divider
+
+printRow: It takes two inputs and prints them as separate columns within a row. It is very helpful if you want to print key-value pairs i.e., product name and sale price.
+
+printBarcode: It prints the barcode and prints the given barcode String beneath the barcode. 
+
+
+## Getting started
+
+You should have a web project as this package only supports Flutter Web for now. Before starting the printing, you need to call the pairDevice() function and provide
+the vendorId, productId, interfaceNumber, and endpointNumber. The vendorId and productId are required and you can find them in the device description or check directly
+from your system by connecting it via USB. The interfaceNumber and endpointNumber are default set as 0 and 1 respectively which works for most cases. However, if it does not
+works, try to find the correct ones and change accordingly.
+
+
+## Usage
+
+The function below prints a sample receipt. NOTE: You may have to change the vendorId, productId, and may also provide interfaceNumber and endpointNumber to the function pairDevice().
+```dart
+//Create an instance of printer
+  WebThermalPrinter _printer = WebThermalPrinter();
+
+// A Dummy Function that you can call on any button and test.
+
+printReceipt()
+async
+{
+  
+  //Pairing Device is required.
+  await _printer.pairDevice(vendorId: 0x6868, productId: 0x0200);
+  
+  await _printer.printText('DKT Mart',
+      bold: true, centerAlign: true);
+  await _printer.printEmptyLine();
+  
+  await _printer.printRow("Products", "Sale");
+  await _printer.printEmptyLine();
+  
+  for (int i = 0; i < 10; i++) {
+    
+    await _printer.printRow('A big title very big title ${i + 1}',
+        '${(i + 1) * 510}.00 AED');
+    await _printer.printEmptyLine();
+    
+  }
+  
+  await _printer.printDottedLine();
+  await _printer.printEmptyLine();
+  
+  await _printer.printBarcode('123456');
+  await _printer.printEmptyLine();
+  
+  await _printer.printEmptyLine();
+  await _printer.closePrinter();
+}
+```
+
+## Additional information
+
+TODO: Tell users more about the package: where to find more information, how to
+contribute to the package, how to file issues, what response they can expect
+from the package authors, and more.
---
+++ lib/usb_thermal_printer_web.dart
@@ -0,0 +1,141 @@
+library usb_thermal_printer_web;
+
+import 'dart:convert';
+import 'dart:math';
+import 'dart:typed_data';
+
+import 'package:usb_device/usb_device.dart';
+
+class WebThermalPrinter {
+  final UsbDevice usbDevice = UsbDevice();
+  var pairedDevice;
+
+  //By Default, it is usually 0
+  var interfaceNumber;
+
+  //By Default, it is usually 1
+  var endpointNumber;
+
+  Future<void> pairDevice(
+      {required int vendorId,
+        required int productId,
+        int? interfaceNo,
+        int? endpointNo}) async {
+    interfaceNumber = interfaceNo ?? 0;
+    endpointNumber = endpointNo ?? 1;
+    pairedDevice ??= await usbDevice.requestDevices(
+        [DeviceFilter(vendorId: vendorId, productId: productId)]);
+    await usbDevice.open(pairedDevice);
+    await usbDevice.claimInterface(pairedDevice, interfaceNumber);
+  }
+
+  Future<void> printRow(
+      String title,
+      String value,
+      ) async {
+    var titleColumnWidth = 18;
+    var valueColumnWidth = 12;
+
+    // Split the title and value into separate rows
+    var titleRows = _splitStringIntoRows(title, titleColumnWidth);
+    var valueRows = _splitStringIntoRows(value, valueColumnWidth);
+
+    // Print each row separately
+    for (var i = 0; i < max(titleRows.length, valueRows.length); i++) {
+      var titleRow = titleRows.length > i ? titleRows[i] : '';
+      var valueRow = valueRows.length > i ? valueRows[i] : '';
+
+      var encodedTitle = utf8.encode(titleRow.padRight(titleColumnWidth));
+      var encodedValue = utf8.encode(valueRow.padLeft(valueColumnWidth));
+
+      var buffer = Uint8List.fromList([
+        ...encodedTitle,
+        ...encodedValue,
+        0x0A, // Line feed
+      ]).buffer;
+
+      await usbDevice.transferOut(pairedDevice, endpointNumber, buffer);
+    }
+  }
+
+  List<String> _splitStringIntoRows(String str, int rowWidth) {
+    var rows = <String>[];
+    var currentRow = '';
+    for (var word in str.split(' ')) {
+      if ((currentRow + word).length > rowWidth) {
+        rows.add(currentRow);
+        currentRow = '';
+      }
+      currentRow += word + ' ';
+    }
+    if (currentRow.isNotEmpty) {
+      rows.add(currentRow);
+    }
+    return rows;
+  }
+
+  Future<void> printBarcode(String barcodeData) async {
+    var barcodeBytes = Uint8List.fromList([
+      0x1d, 0x77, 0x02, // Set barcode height to 64 dots (default is 50 dots)
+      0x1d, 0x68, 0x64, // Set barcode text position to below barcode
+      0x1d, 0x48, 0x02, // Set barcode text font to Font B (default is Font A)
+      0x1d, 0x6b, 0x49, // Print Code 128 barcode with text
+      barcodeData.length + 2, // Length of data to follow (barcodeData + {B})
+      0x7b, 0x42, // Start Code B
+    ]);
+    var barcodeStringBytes = utf8.encode(barcodeData);
+    var data = Uint8List.fromList([...barcodeBytes, ...barcodeStringBytes]);
+
+    var centerAlignBytes = Uint8List.fromList([
+      0x1b, 0x61, 0x01, // Center align
+    ]);
+    var centerAlignData = centerAlignBytes.buffer.asByteData();
+    var resetAlignBytes = Uint8List.fromList([
+      0x1b, 0x61, 0x00, // Reset align to left
+    ]);
+    var resetAlignData = resetAlignBytes.buffer.asByteData();
+
+    await usbDevice.transferOut(
+        pairedDevice, endpointNumber, centerAlignData.buffer);
+    await usbDevice.transferOut(pairedDevice, endpointNumber, data.buffer);
+    await usbDevice.transferOut(
+        pairedDevice, endpointNumber, resetAlignData.buffer);
+  }
+
+  Future<void> printText(
+      String text, {
+        bool? bold,
+        bool centerAlign = false,
+      }) async {
+    var encodedText =
+    utf8.encode((bold ?? false) ? "\x1B[$text\x1B\n" : "$text\n");
+    if (centerAlign) {
+      var width = 28; // Change this to adjust the width of the printer
+      var leftPadding = ((width - text.length) / 2).floor();
+      var rightPadding = width - text.length - leftPadding;
+      var paddingString =
+          ''.padLeft(leftPadding) + text + ''.padRight(rightPadding);
+      encodedText = utf8.encode("\n$paddingString\n");
+    }
+    var buffer = Uint8List.fromList(encodedText).buffer;
+
+    await usbDevice.transferOut(pairedDevice, endpointNumber, buffer);
+  }
+
+  Future<void> printEmptyLine() async {
+    var encodedText = utf8.encode("\n");
+    var buffer = Uint8List.fromList(encodedText).buffer;
+    await usbDevice.transferOut(pairedDevice, endpointNumber, buffer);
+  }
+
+  Future<void> printDottedLine() async {
+    var encodedText = utf8.encode("\n--------------------------------\n");
+    var buffer = Uint8List.fromList(encodedText).buffer;
+    await usbDevice.transferOut(pairedDevice, endpointNumber, buffer);
+  }
+
+  Future<void> closePrinter() async {
+    await usbDevice.close(pairedDevice);
+  }
+}
+
